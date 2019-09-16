9f2a2f52e3e154a7ae7a60176eec2b4d682eb5a6
Dmitry Sinelnikov <dsinelnikov96@gmail>
2019-09-16 21:46:48 +0300

    In development

--- colors.py
+++ colors.py
@@ -1,7 +1,15 @@
 import sys
 
-for i in range(0, 16):
-    for j in range(0, 16):
-        code = str(i * 16 + j)
-        sys.stdout.write(u"\u001b[38;5;" + code + "m " + (code + ' ' + sys.argv[1]).ljust(6))
-    print(u"\u001b[0m")
+
+def main():
+    symbol = sys.argv[1].encode().decode("unicode-escape")
+    for i in range(0, 16):
+        for j in range(0, 16):
+            code = str(i * 16 + j)
+            sys.stdout.write(u"\u001b[38;5;" + code + "m ")
+            sys.stdout.write((code + ' ' + symbol).ljust(6))
+        print(u"\u001b[0m")
+
+
+if __name__ == "__main__":
+    main()
