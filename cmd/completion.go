85615db88bef0bcd7007c297fd1ecd4df5d544e8
Dmitry Sinelnikov <dsinelnikov96@gmail.com>
2019-10-04 20:03:13 +0300

    Zsh completion generation (#9)
    #2 zsh completion

--- cmd/completion.go
+++ cmd/completion.go
@@ -8,26 +8,37 @@ import (
 )
 
 var completionCmd = &cobra.Command{
-	Use:   "completion bash",
+	Use:   "completion bash|zsh",
 	Short: "Generates completion scripts for the given shell",
 	Long: `Generates completion scripts for the given shell
 
-To load bash completion run
-
+To load bash completion run:
 . <(izer completion bash)
 
-To configure your bash shell to load completions for each session add to your bashrc
+To load zsh completion run:
+. <(izer completion zsh)
+
+To configure your shell to load completions for each session
+add this to your shell rc file:
 
 # ~/.bashrc or ~/.profile
 . <(izer completion bash)
+
+# ~/.zshrc
+. <(izer completion zsh)
 	`,
-	ValidArgs: []string{"bash"},
+	ValidArgs: []string{"bash", "zsh"},
 	Args:      cobra.ExactValidArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		switch args[0] {
 		case "bash":
 			if err := rootCmd.GenBashCompletion(os.Stdout); err != nil {
 				fmt.Printf("Cannot generate bash completion: %v\n", err)
+				os.Exit(1)
+			}
+		case "zsh":
+			if err := rootCmd.GenZshCompletion(os.Stdout); err != nil {
+				fmt.Printf("Cannot generate zsh completion: %v\n", err)
 				os.Exit(1)
 			}
 		}
